fdad5a3b6c2f23651d22ec725f415ab44fc9f465
DAGA <DAGA@DAGA-PC.eam.edu.co>
2018-06-20 09:41:27 -0500

    Modificacion tutorial

--- View/public/home.php
+++ View/public/home.php
@@ -7,8 +7,22 @@
             <h5 class="header col s12 light">Análisis de datos web EAM para la toma de decisiones</h5>
         </div>
         <div class="row center">
-            <a href="" id="download-button" 
-               class="btn-large waves-effect waves-light grisEAM">Tutorial</a>
+            <a href="#modalTutorial" class="grisEAM waves-effect waves-light btn modal-trigger" >Tutorial</a>
+
+            <a class="waves-effect waves-light btn modal-trigger" href="#modal1">Modal</a>
+
+            <!-- Modal Structure -->
+            <div id="modal1" class="modal modal-fixed-footer"><!--La clase modal-fixed-footer es para ajustar el texto cuando es demasiado largo-->
+                <div class="modal-content">
+                    <h4>Modal Header</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+                </div>
+                <div class="modal-footer">
+                    <a href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat">Agree</a>
+                </div>
+            </div>
+
+
         </div>        
 
     </div>
@@ -92,7 +106,31 @@
     <br><br>
 </div>
 
+<script>
+    $(document).ready(function () {
+        // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+        $('.modal-trigger').leanModal();
 
+
+        //Para programarlos
+        //$('#modal1').openModal();//ABRIR MODAL
+        //  $('#modal1').closeModal();//CERRAR MODAL
+
+        //configuracion completa
+        /*
+         
+         $('.modal-trigger').leanModal({
+         dismissible: true, // Modal can be dismissed by clicking outside of the modal
+         opacity: .5, // Opacity of modal background
+         in_duration: 300, // Transition in duration
+         out_duration: 200, // Transition out duration
+         ready: function() { alert('Ready'); }, // Callback for Modal open
+         complete: function() { alert('Closed'); } // Callback for Modal close
+         }
+         ); 
+         */
+    });
+</script>
 
 
 
